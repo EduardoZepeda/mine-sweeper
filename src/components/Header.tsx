1a90aad503baba6d9058446ba73e23b52164f56a
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-06-10 18:11:02 -0500

    Remove unused imports and context

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,9 @@
 
 import React, { useContext } from 'react'
 import styles from '../Header.module.css'
-import { GameStateContext } from '../context/gameState'
 import ResetButton from './ResetButton'
 
 const Header = (): JSX.Element => {
-  const { gameState, setGameState } = useContext(GameStateContext)
 
   return (
     <header className={styles.header}>
