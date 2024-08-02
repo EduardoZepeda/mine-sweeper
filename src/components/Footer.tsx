fbe98eb42bffa2301ae9c3f3adab23d580cc64c8
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2024-08-01 18:25:24 -0600

    Update author's personal page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
 import styles from '../Footer.module.css'
 
 const Footer = (): JSX.Element => {
   return (
-    <footer className={styles.footer}><small>Coded with ♥ by <a href="https://eduardozepeda.dev">Eduardo Zepeda</a></small></footer >
+    <footer className={styles.footer}><small>Coded with ♥ by <a href="https://zeedu.dev">Eduardo Zepeda</a></small></footer >
   )
 }
 
